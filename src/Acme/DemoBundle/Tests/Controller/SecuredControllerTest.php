63d2d32a28df27eeef67b69aa61c17fb2bedfaac
Belousov Alexey <forgottenbas@gmail.com>
2013-01-27 23:45:00 +0600

    found a way around a to authorize with single request

--- src/Acme/DemoBundle/Tests/Controller/SecuredControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/SecuredControllerTest.php
@@ -4,18 +4,26 @@ namespace Acme\DemoBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * phpunit -c app/ src/Acme/DemoBundle/Tests/Controller/SecuredControllerTest.php
  */
 class SecuredControllerTest extends WebTestCase
 {
-    public function testAuthorizeWithSession()
+    public function testAuthorizeWithSession2Request()
     {
         $client = static::createClient();
         $client->followRedirects(false);
+
+        $container = $client->getContainer();
+
+        /** @var $security \Symfony\Component\Security\Core\SecurityContext */
+        $security = $container->get('security.context');
+        /** @var $event_dispatcher \Symfony\Component\HttpKernel\Debug\ContainerAwareTraceableEventDispatcher */
+        $event_dispatcher = $container->get('event_dispatcher');
+        /** @var $session  \Symfony\Component\HttpFoundation\Session\Session */
         $session = $client->getContainer()->get('session');
+
         $client->getCookieJar()->set(new \Symfony\Component\BrowserKit\Cookie($session->getName(), $session->getId()));
 
         # fake request
@@ -42,5 +50,44 @@ class SecuredControllerTest extends WebTestCase
             $crawler->filter('h1:contains("Hello Alexey secured for Admins only!")')->count()
         );
 
+    }
+
+    public function testAuthorizeWithSession1Request()
+    {
+        $client = static::createClient();
+        $client->followRedirects(false);
+
+        $container = $client->getContainer();
+
+        /** @var $security \Symfony\Component\Security\Core\SecurityContext */
+        $security = $container->get('security.context');
+        /** @var $event_dispatcher \Symfony\Component\HttpKernel\Debug\ContainerAwareTraceableEventDispatcher */
+        $event_dispatcher = $container->get('event_dispatcher');
+        /** @var $session  \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $client->getContainer()->get('session');
+
+        $session->start();
+
+        $client->getCookieJar()->set(new \Symfony\Component\BrowserKit\Cookie($session->getName(), $session->getId()));
+
+        $token = new UsernamePasswordToken('admin', 'adminpass', 'secured_area', array());
+        $session->set('_security_secured_area', serialize($token));
+        $session->save();
+
+        $client->request('GET', '/demo/secured/hello/Alexey');
+
+        $this->assertEquals(
+            $client->getResponse()->getStatusCode(),
+            200
+        );
+
+        $client->request('GET', '/demo/secured/hello/admin/Alexey');
+
+        echo $client->getResponse()->getContent();
+
+        $this->assertEquals(
+            $client->getResponse()->getStatusCode(),
+            200
+        );
     }
 }
